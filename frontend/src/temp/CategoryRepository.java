311a77dffe191346934b60b35cd20b8c9ef3881a
SHIV SUNDER DASH <22053627@kiit.ac.in>
2025-06-20 13:00:39 +0530

    Delete frontend/src/temp directory

--- frontend/src/temp/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fashionhub.repository;
-
-import com.fashionhub.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {}
